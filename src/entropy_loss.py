986cada06b3c0ceeaf50730d5c9d7433d7ded8cd
gkopte <gabrielkopte@gmail.com>
2023-04-25 20:50:34 -0300

    implementando self_loss utilizando velocidade (x-x_last) ai inves de posição (x).

--- src/entropy_loss.py
+++ src/entropy_loss.py
@@ -135,18 +135,13 @@ def self_loss (x, fx_array, n,im_loss_option):
 		print(first_instance)
 
 		#creating tensor for pso x and coping initial x to it
-		# x_pso = tf.Variable(tf.zeros(tf.shape(first_instance)), dtype=tf.float32)
-		# x_pso.assign(first_instance)
+		x_pso = tf.Variable(tf.zeros(tf.shape(first_instance)), dtype=tf.float32)
+		x_pso.assign(first_instance)
 
-		# Get the shape of the existing tensor
-		first_instance_shape = first_instance.shape
-		x_pso = tf.random.uniform(
-			first_instance_shape,
-			minval=-3, 
-			maxval=3, 
-		    name='x_pso',
-			dtype=tf.float32,
-		)	
+		x_last = tf.Variable(tf.zeros(tf.shape(x)), dtype=tf.float32)
+		x_pso_hist_last = tf.Variable(tf.zeros(tf.shape(x)), dtype=tf.float32)
+		x_vel = tf.Variable(tf.zeros(tf.shape(x)), dtype=tf.float32)
+		# vel_pso_x_history = tf.Variable(tf.zeros(tf.shape(x)), dtype=tf.float32)
 		
 		# building pso graph 
 		print('n: ', n)
@@ -154,13 +149,11 @@ def self_loss (x, fx_array, n,im_loss_option):
 		pso_.train()
 
 		# getting pso x history to calculate loss
-		# pso_x_history = tf.reshape(pso_.x_history[1::], (batch_size, n, problem_dim))
+		pso_x_history = tf.reshape(pso_.x_history[1::], (batch_size, n, problem_dim))
 
-		# Reshape the existing tensor to shape (1, 1, 5)
-		reshaped_g_best = tf.reshape(pso_.g, [1, 1, problem_dim])
-
-		# Repeat the tensor along the desired dimensions (128, 147, 5)
-		stacked_g_best = tf.tile(reshaped_g_best, [batch_size, n, 1])
+		# velocity calculation
+		vel_pso_x_history = tf.stop_gradient(pso_x_history) - tf.stop_gradient(x_pso_hist_last)
+		x_vel = tf.stop_gradient(x) - x_last
 
 		def custom_loss(y_true, y_pred):
 			z = tf.abs(y_true - y_pred)
@@ -169,16 +162,19 @@ def self_loss (x, fx_array, n,im_loss_option):
 			return tf.reduce_mean(tf.where(z >= 1.0, quadratic, absolute))
 		
 		if option=='custom':
-			im_loss = custom_loss(stacked_g_best,x)
+			im_loss = custom_loss(vel_pso_x_history, x_vel)
 		elif option=='rmse':
-			im_loss = tf.sqrt(tf.reduce_mean(tf.square(stacked_g_best - x)))
+			im_loss = tf.sqrt(tf.reduce_mean(tf.square(vel_pso_x_history - x_vel)))
 		elif option=='huber':
 			huber_loss = tf.keras.losses.Huber(delta=1.0)
-			im_loss = huber_loss(stacked_g_best, x)
+			im_loss = huber_loss(vel_pso_x_history, x_vel)
 		elif option=='mse': 
-			im_loss = tf.reduce_mean(tf.reduce_mean((stacked_g_best - x)**2,0))
+			im_loss = tf.reduce_mean(tf.reduce_mean((vel_pso_x_history - x_vel)**2,0))
 		else: #sumed square
-			im_loss = tf.reduce_sum(tf.reduce_sum((stacked_g_best - x)**2,0))
+			im_loss = tf.reduce_sum(tf.reduce_sum((vel_pso_x_history - x_vel)**2,0))
+		
+		x_pso_hist_last.assign(pso_x_history)
+		x_last.assign(x)
 		return im_loss
 	
 	# im_loss_option = 'mse'
@@ -251,7 +247,7 @@ def self_loss (x, fx_array, n,im_loss_option):
 		print("sumfx shape:", sumfx.shape)
 		return sumfx+im_loss*k
 	elif im_loss_option=='only_im':
-		im_loss = imitation_error(x, fx_array, n,'square')
+		im_loss = imitation_error(x, fx_array, n,'mse')
 		print("im_loss shape:", im_loss.shape)
 		print("sumfx shape:", sumfx.shape)
 		return im_loss*k
